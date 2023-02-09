98b91e99cb32bf7358a4bc2e212e8b07611f78bf
Timur <serjasum1994@icloud.com>
2023-02-09 21:58:05 +0500

    Увеличен лимит запросов. Добавлена Dashboard.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import flask_monitoringdashboard as dashboard
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_toastr import Toastr
@@ -5,11 +6,12 @@ from flask_login import LoginManager
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+
 app = Flask(__name__)
 limiter = Limiter(
     get_remote_address,
     app=app,
-    default_limits=["30 per minute", "5 per second"],
+    default_limits=["30 per minute"],
     storage_uri="memory://"
 )
 app.config["SECRET_KEY"] = "fAZmf248XfG"
@@ -17,3 +19,4 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 toastr = Toastr(app)
 manager = LoginManager(app)
+dashboard.bind(app)
